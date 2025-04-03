d57b7ab852abafc33b01a0eddf463918bfdbe8f3
lile <1250124009@qq.com>
2025-04-03 20:05:51 +0800

    feat：函数相关代码

---
+++ codes/function/experience_module.py
@@ -0,0 +1,5 @@
+import divider_module
+
+divider_module.print_line("M", 6)
+
+print(divider_module.name)
---
+++ codes/function/import_func.py
@@ -0,0 +1,4 @@
+# 导入工具包
+import multiplication_table
+# 调用工具包中的函数
+multiplication_table.multiple_table()
